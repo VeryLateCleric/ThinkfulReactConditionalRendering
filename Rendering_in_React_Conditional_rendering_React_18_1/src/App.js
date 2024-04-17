b9ee5dc974d9a5a178d2eb468d81e1fcc6f10383
Kenneth <kennethyoung.2960@gmail.com>
2024-04-16 20:58:26 -0500

    Clock finished, 4 of 5 tests passing

--- Rendering_in_React_Conditional_rendering_React_18_1/src/App.js
+++ Rendering_in_React_Conditional_rendering_React_18_1/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Clock from "./Clock";
 
 function App(props) {
+
   // Write your code here.
   // Hint: It's not necessary to pass props to clock as an argument.
   const { loggedIn } = props;
--- Rendering_in_React_Conditional_rendering_React_18_1/src/Clock.js
+++ Rendering_in_React_Conditional_rendering_React_18_1/src/Clock.js
@@ -2,6 +2,19 @@ import React from "react";
 
 function Clock() {
   // Write your code here.
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+
+  let timeOfDay;
+  if (currentHour < 12) {
+    timeOfDay = "Good Morning!";
+  } else if (currentHour < 18) {
+    timeOfDay = "Good Afternoon!";
+  } else {
+    timeOfDay = "Good Evening!";
+  }
+
+  return <div>{timeOfDay}</div>;
 }
 
 export default Clock;
